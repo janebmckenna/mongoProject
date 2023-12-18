7e834cd29433f65c899776ed1153926041a936f6
janebmckenna <>
2023-12-18 16:52:26 +0000

    update login function

--- app.py
+++ app.py
@@ -49,6 +49,27 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists in DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure passwords match
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # passwords dont match
+                flash ("Incorrect username and/or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesnt exist
+            flash ("Incorrect username and/or password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 if __name__ == "__main__":
