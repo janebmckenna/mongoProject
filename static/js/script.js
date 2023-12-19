0d757bcfcf328b0a65cdb4274626b19785f34d08
jane mckenna <janemckenna@hotmail.com>
2023-12-19 18:15:38 +0000

    Feat: add categories drop down

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge:"right"});
-    $('.collapsible').collapsible();
-    $('.tooltip').tooltip('methodName');
-    $('.tooltip').tooltip('methodName', paramName);
-    $('.datepicker').datepicker({
+// $(document).ready(function(){
+//   $('.sidenav').sidenav({edge:"right"});
+//   $('.collapsible').collapsible();
+//   $('.tooltip').tooltip('methodName');
+//   $('.tooltip').tooltip('methodName', paramName);
+//   $('select').formSelect();
+//   $('.datepicker').datepicker({
+//     format: "dd mmmm, yyyy",
+//     yearRange: 3,
+//     showClearBtn: true,
+//     i18n: {
+//       done: select
+//     }
+//   });
+// });
+
+$(document).ready(function () {
+  $(".sidenav").sidenav({edge: "right"});
+  $(".collapsible").collapsible();
+  $(".tooltipped").tooltip();
+  $("select").formSelect();
+  $(".datepicker").datepicker({
       format: "dd mmmm, yyyy",
       yearRange: 3,
       showClearBtn: true,
       i18n: {
-        done: select
+          done: "Select"
       }
-    });
   });
-
+});
