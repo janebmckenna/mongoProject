2e150457a65abee146447f6baeca1378adc95711
jane mckenna <janemckenna@hotmail.com>
2023-12-19 17:08:34 +0000

    Feat: add form for add task

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.tooltip').tooltip('methodName');
     $('.tooltip').tooltip('methodName', paramName);
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: select
+      }
+    });
   });
+
